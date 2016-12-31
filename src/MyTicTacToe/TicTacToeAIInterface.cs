0e87b75b0e32ce02dde79fa381188820a843c502
masjinno <masjinno@gmail.com>
2016-12-31 16:50:01 +0900

    #6 AIのインターフェースを追加

---
+++ src/MyTicTacToe/TicTacToeAIInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTicTacToe
+{
+    interface TicTacToeAIInterface
+    {
+        Tuple<int, int> PutMark(TicTacToeLogic.TicTacToeMark.MarkNum[,] boardData, TicTacToeLogic.TicTacToeMark.MarkNum mark);
+    }
+}
