530f6fdee8874414acdd9082886065fcbad04132
masjinno <masjinno@gmail.com>
2017-01-02 14:16:12 +0900

    #6 コメントを追加

--- src/MyTicTacToe/TicTacToeAIInterface.cs
+++ src/MyTicTacToe/TicTacToeAIInterface.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace MyTicTacToe
 {
+    /// <summary>
+    /// TicTacToeのAIインターフェース。
+    /// staticなクラスとして実装されることを想定する。
+    /// </summary>
     interface ITicTacToeAIInterface
     {
+        /// <summary>
+        /// boardDataに対して、markを打つ場所を返す静的メソッドのインターフェース
+        /// </summary>
+        /// <param name="boardData">盤面データ</param>
+        /// <param name="mark">打つマーク</param>
+        /// <returns>
+        /// Item1 : 横位置 (boarData[Item1, Item2])
+        /// Item2 : 縦位置 (boarData[Item1, Item2])
+        /// </returns>
         Tuple<int, int> PutMark(TicTacToeLogic.TicTacToeMark.MarkNum[,] boardData, TicTacToeLogic.TicTacToeMark.MarkNum mark);
     }
 }
