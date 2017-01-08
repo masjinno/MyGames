59e206bb4f3cee02f7c718f83971ec56aa8ff56f
masjinno <masjinno@gmail.com>
2017-01-09 04:10:18 +0900

    #5 AI管理クラスを作成。

---
+++ src/MyTicTacToe/AIManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTicTacToe
+{
+    class AIManager
+    {
+        /// <summary>
+        /// AI名
+        /// </summary>
+        private string[] aiNames = { "AI:Random1", "AI:Random2" };
+
+        /// <summary>
+        /// AI実態
+        /// </summary>
+        private ITicTacToeAI[] TicTacToeAI;
+
+        /// <summary>
+        /// 現ターン時にAIが有効か否か
+        /// </summary>
+        public bool IsEnabled {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public AIManager()
+        {
+            TicTacToeAI = new ITicTacToeAI[]
+            {
+                new TicTacToeAIRandom(),
+                new TicTacToeAIRandom()
+            };
+            IsEnabled = false;
+        }
+
+        /// <summary>
+        /// AI名ゲッタ
+        /// </summary>
+        /// <returns>AI名</returns>
+        public string[] GetAiNames()
+        {
+            return aiNames;
+        }
+
+        /// <summary>
+        /// AIの打った場所を取得する。
+        /// </summary>
+        /// <param name="aiIndex">AIインデックス</param>
+        /// <param name="gameData">盤面データとターンを保持するゲームデータ</param>
+        /// <returns>指定された場所</returns>
+        public Tuple<int, int> GetAiPutPlace(int aiIndex, TicTacToeData gameData)
+        {
+            if (aiIndex < 0 || aiNames.Count() <= aiIndex)
+            {
+                return Tuple.Create<int, int>(-1, -1);
+            }
+
+            Tuple<int, int> ret = TicTacToeAI[aiIndex].PutMark(gameData.GetBoard(), gameData.GetTurn());
+            return ret;
+        }
+    }
+}
