91de70d98cc35df81a471ecd0ea91acdd0e4cf70
masjinno <masjinno@gmail.com>
2017-03-11 23:09:01 +0900

    #16 設定データクラスを追加

---
+++ src/MyGames/Resource/MyGamesSettingData.cs
@@ -0,0 +1,15 @@
+﻿namespace MyGames.Resource
+{
+    public class MyGamesSettingData
+    {
+        /// <summary>
+        /// VisualStudio上でデバッグするときに参照するデフォルトゲームイメージのパス
+        /// </summary>
+        public static readonly string DefaultGameImageDebugPath = @"..\..\Resource\NoImage.png";
+
+        /// <summary>
+        /// ゲームとランチャーをコピーした実行環境でのデフォルトゲームイメージのパス
+        /// </summary>
+        public static readonly string DefaultGameImageCopiedEnvironmentPath = @".\NoImage.png";
+    }
+}
