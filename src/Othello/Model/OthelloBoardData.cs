56c377b709f787c87248bd5b187f7e6a05b6656c
masjinno <masjinno@gmail.com>
2017-03-17 07:47:52 +0900

    #7 ロジック、データ、VM等のためのクラスを追加

---
+++ src/Othello/Model/OthelloBoardData.cs
@@ -0,0 +1,65 @@
+﻿using Othello.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello.Model
+{
+    class OthelloBoardData
+    {
+        /// <summary>
+        /// 盤面データ
+        /// </summary>
+        private SettingData.OthelloMark[,] _boardMark;
+        public SettingData.OthelloMark[,] BoardMark
+        {
+            get { return _boardMark; }
+            set { _boardMark = value; }
+        }
+        
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public OthelloBoardData()
+        {
+            InitializeData();
+        }
+
+        /// <summary>
+        /// データ初期化
+        /// </summary>
+        private void InitializeData()
+        {
+            BoardMark = new SettingData.OthelloMark[SettingData.COLUMN_SIZE, SettingData.ROW_SIZE];
+        }
+
+        /// <summary>
+        /// 盤面をゲーム開始時の状態に初期化する
+        /// </summary>
+        public void ResetData()
+        {
+            // 全て空で埋める
+            for (int y = 0; y < SettingData.ROW_SIZE; y++)
+            {
+                for (int x = 0; x < SettingData.COLUMN_SIZE; x++)
+                {
+                    BoardMark[x, y] = SettingData.OthelloMark.None;
+                }
+            }
+
+            // 縦マス数・横マス数は偶数の前提で、初期配置マスを決定
+            int CenterXLeft = SettingData.COLUMN_SIZE / 2 - 1;
+            int CenterXRight = SettingData.COLUMN_SIZE / 2;
+            int CenterYUpper = SettingData.ROW_SIZE / 2 - 1;
+            int CenterYLower = SettingData.ROW_SIZE / 2;
+
+            // 初期配置を入力
+            BoardMark[CenterXLeft, CenterYLower] = SettingData.OthelloMark.Black;
+            BoardMark[CenterXRight, CenterYUpper] = SettingData.OthelloMark.Black;
+            BoardMark[CenterXLeft, CenterYUpper] = SettingData.OthelloMark.White;
+            BoardMark[CenterXRight, CenterYLower] = SettingData.OthelloMark.White;
+        }
+    }
+}
---
+++ src/Othello/ViewModel/ViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Othello.Resource;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello.ViewModel
+{
+    class ViewModel : BindableBase
+    {
+        #region Binding用のプロパティ
+
+        private SettingData.OthelloMark[,] _boardMark;
+        public SettingData.OthelloMark[,] BoardMark
+        {
+            get
+            {
+                return _boardMark;
+            }
+            set
+            {
+                SetProperty(ref _boardMark, value);
+            }
+        }
+
+        private int _blackCount;
+        public int BlackCount
+        {
+            get
+            {
+                return _blackCount;
+            }
+            set
+            {
+                SetProperty(ref _blackCount, value);
+            }
+        }
+
+        private int _whiteCount;
+        public int WhiteCount
+        {
+            get
+            {
+                return _whiteCount;
+            }
+            set
+            {
+                SetProperty(ref _whiteCount, value);
+            }
+        }
+        #endregion
+
+        public ViewModel()
+        {
+            
+        }
+    }
+}
